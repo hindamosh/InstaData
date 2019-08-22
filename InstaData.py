b2bf5cb88317f80a843f753faebe0a0e9aca801c
Hindam <mohamed.hendam@ymail.com>
2019-08-22 18:15:52 +0300

    update the script using pandas 0.25

--- InstaData.py
+++ InstaData.py
@@ -1,144 +1,114 @@
-import json
-import requests
-import datetime
 import pandas as pd
+import requests
 from time import sleep
-import os
-from collections import Counter
 
-def hashtag_data(hashtag):
+def data_posts_count(searched_tag):
+    'to get primary dataframe for the seareched tag and its total posts count'
+    url = 'https://www.instagram.com/explore/tags/'+ searched_tag + '/?__a=1'
+    response = requests.get(url)
+    data = response.json()
+    total_posts_count = data['graphql']['hashtag']['edge_hashtag_to_media']['count']
+    return data , total_posts_count
+
+def extract_tags(x):
+    'to extract hashtags/keywords from a text x if any'
+    if x is pd.np.nan:
+        return ''
+    return ','.join(''.join([i for i in x.split() if i.startswith('#')]).split('#')[1:])
+
+def get_temp_df(data):
+    'to get temporary dataframe'
+    # top_posts and other posts
+    top_posts = data['graphql']['hashtag']['edge_hashtag_to_top_posts']['edges']
+    posts = data['graphql']['hashtag']['edge_hashtag_to_media']['edges']
+    # get the node where data located
+    clean_top_posts = [i['node'] for i in top_posts ]
+    clean_posts = [i['node'] for i in posts  ]
+    # select the interested columns
+    columns = ['owner', 'shortcode', 'taken_at_timestamp',  'edge_media_to_caption' , 'edge_media_to_comment', \
+                       'edge_liked_by', 'is_video', 'video_view_count']
+    # set up the dataframe
+    top_posts_df = pd.DataFrame(clean_top_posts, columns = columns)
+    top_posts_df['is_top'] = True
+    posts_df = pd.DataFrame(clean_posts,columns = columns )
+    posts_df['is_top'] = False
+    temp_df = top_posts_df.append(posts_df, sort=False)
+    return temp_df
+
+def insta_scraper(searched_tag):
     """
     colllect the needed data for the interested hashtag
     ===================================================
     INPUT:
-    hashtag : str
+    searched_tag : str
     ---------------------------------------------------
     OUTPUT:
-    tags_data : Pandas DataFrame with 
-               -hashtag : the name of the requested hashtag  ,str
-               -related key words: related hashtags written in the post, list 
-               -number of comments:
-               -number of likes:
-               -date of post: datetime
-               -is_vedio : True or False
-               -is_top : to be one of the top posts , True or False
-               
-    {hashtag}_data.csv file : convert pandas DataFrame to .csv file
-
+    {searched_tag}_data.csv file 
+    data_df  : Pandas DataFrame with 
+             -'user_id': user id number, str
+             -'url':post link, str
+             -'date': date of post , datetime64[ns]
+             -'text': post text , str 
+             -'num_comments': number of comments , int64
+             -'num_likes': number of likes , int64
+             -'is_video':is vedio post or not , bool
+             -'video_view_count':number of vedio views if is_vedio: True , float64
+             -'is_top': if the post is top post , bool
+             -'other_tags' related hashtags/key words
     """
-    # get the json link for the needed hashtag
-    url = 'https://www.instagram.com/explore/tags/'+ hashtag + '/?__a=1'
-    response = requests.get(url)
-    response_text = response.text
-    data = json.loads(response_text)
-
-    #loop through the old posts
-    all_data = []
-    all_data.append(data)
-    
+    # url
+    url = 'https://www.instagram.com/explore/tags/'+ searched_tag + '/?__a=1'
+    # get the previous data and total posts count
+    data , total_posts_count = data_posts_count(searched_tag)
+    print(f'There is {total_posts_count:,} posts for #{searched_tag}')
+    # set up the dataframe
+    data_df = get_temp_df(data)
+    # has next page (True or False)
     has_next_page = data['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['has_next_page']
+    # get how many posts to scrape
+    deep= int(input('Enter the number of posts you want to scrape:'))
     # get all data
-    while has_next_page:
-        # end cursor to be put after (max_id = ) to go to next page
-        next_page = data['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor']
-        new_url = url + '&max_id=' + next_page
-        # parse the interested url
-        sleep(2)
-        response = requests.get(new_url)
-        response_text = response.text
-        # load json file
-        data = json.loads(response_text)
-        all_data.append(data)
-        has_next_page = data['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['has_next_page']
-
-    #get post_details
-    links,num_comments,num_likes,post_dates,is_vedios,is_tops,x_tags =[],[],[],[],[],[],[]
-    #loop through all collected data
-    for data in all_data:
-        # arrange the data needed either from top_posts or normal posts 
-        top_posts = data['graphql']['hashtag']['edge_hashtag_to_top_posts']['edges']
-        posts = data['graphql']['hashtag']['edge_hashtag_to_media']['edges']
-        all_posts = [top_posts,posts]
-        for i in all_posts:
-            if i is top_posts:
-                # is_top
-                is_top = True
-            else:
-                is_top = False
-                
-            # looping through both lists at once  
-            for _ in i:
-                # the post content
-                post = _['node']
-                # other hashtags/key words appear on the post
-                try:
-                    text = post['edge_media_to_caption']['edges'][0]['node']['text']
-                    x_tag = [i for i in text.split() if i.startswith('#')] 
-                except Exception:
-                    continue
-                # link to the post
-                post_link = 'https://www.instagram.com/p/'+post['shortcode'] + '/'
-                # number of comments on the post
-                num_comment = post['edge_media_to_comment']['count']
-                # number of likes 
-                num_like = post['edge_liked_by']['count']
-                # the post date
-                post_date = datetime.datetime.fromtimestamp(post['taken_at_timestamp']).strftime("%Y-%m-%d")
-                # vedio
-                is_vedio = post['is_video']
-                # gather other-tags data
-                index = 0
-                while index < len(x_tag):
-                    x_tag[index] = x_tag[index].strip('#')
-                    index += 1
-                    x_tag_str = ','.join(x_tag)
-                x_tags.append(x_tag_str)
-                #if len(x_tag) > 0:
-                #    x_tags.append(x_tag[0].split('\n')[-1])
-                #else:
-                #    x_tags.append('')
-                links.append(post_link)
-                num_comments.append(num_comment)
-                num_likes.append(num_like)
-                post_dates.append(post_date)
-                is_vedios.append(is_vedio)
-                is_tops.append(is_top)
-                              
-    tags_data = pd.DataFrame({'hashtag':hashtag,'other_tags':x_tags,'num_comments':num_comments, 
-                          'num_likes':num_likes,'date':post_dates,'is_vedios':is_vedios,
-                          'is_top_post':is_tops,'link':links})
-    # replace the empty string eith none values
-    tags_data.other_tags = tags_data.other_tags.apply(lambda x : None if x == '' else x)
-    tags_data.other_tags = tags_data.other_tags.apply(lambda x : x.replace('#',','))
-    # clean other_tags and create a nother dataFrame for other_tags
-    tag_counter = Counter()
-    for i in tags_data.other_tags:
-        for tag in  i.split(','):
-            tag_counter[tag] += 1
-
-    other_tags_df = pd.DataFrame({'tag':list(tag_counter.keys()),'tag_count':list(tag_counter.values())})
-    # replace empty string with null
-    other_tags_df.tag = other_tags_df.tag.apply(lambda x : None if x == '' else x)
-    # rearrange data by tag_count
-    other_tags_df.sort_values(by='tag_count', ascending=False,inplace=True)
-
+    while has_next_page :
+        if len(data_df)>= deep:
+            break
+        else:
+            # end cursor to be put after (max_id = ) to go to next page
+            next_page = data['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor']
+            new_url = url + '&max_id=' + next_page
+            # parse the interested url
+            sleep(2)
+            response = requests.get(new_url)
+            # data from json file
+            data = response.json()            
+            has_next_page = data['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['has_next_page']
+            temp_df = get_temp_df(data)
+            data_df = temp_df.append(data_df,sort=False)
+    data_df.reset_index(drop =True, inplace=True)
+    
+    # rename columns
+    data_df.rename(columns={"edge_media_to_caption": "text", "edge_media_to_comment": "num_comments", \
+                  "taken_at_timestamp":"date" , "edge_liked_by": "num_likes", \
+                  "owner":"user_id", "is_video":"is_video",\
+                  "video_view_count": "video_view_count" , 'shortcode':'url'},inplace=True)
+    
+    # fix the columns
+    data_df.user_id = data_df.user_id.apply(lambda x: x['id'])
+    data_df.url = data_df.url.apply(lambda x :'https://www.instagram.com/p/'+ x + '/')
+    data_df.date = pd.to_datetime(data_df.date, unit='s')
+    data_df.text = data_df.text.apply(lambda x: x['edges'][0]['node']['text'] if len(x['edges']) == 1 else pd.np.nan)
+    data_df.num_comments = data_df.num_comments.apply(lambda x: x['count'])
+    data_df.num_likes = data_df.num_likes.apply(lambda x: x['count'])
+    # get other tags
+    data_df['other_tags'] = data_df.text.apply(lambda x: extract_tags(x))
+    
     #save to csv file to the working directory
-    path_to_data = f'{hashtag}_data.csv'
-    path_to_others = f'{hashtag}_related_tags.csv'
-    # if needed to be saved to a certain directory uncomment the following
-    #path = r""
-    #path_to_data = os.path.join(path,path_to_data) 
-    #path_to_others = os.path.join(path,path_to_others) 
-
+    path_to_data = f'{searched_tag}_data.csv'
     # save data
-    tags_data.to_csv(path_to_data, sep=',', header=True, encoding='utf-8', index= False)
-    # save the other_hashtags data
-    other_tags_df.to_csv(path_to_others, sep=',', header=True, encoding='utf-8',index =False)
-
-    return tags_data , other_tags_df
+    data_df.to_csv(path_to_data, encoding='utf-8-sig', index= False)
+    return data_df
 
 if __name__ == "__main__":
-    hashtag = str(input('Insert the needed hashatag...:'))
+    tag = str(input('Enter the hashtag:'))
     print('Collecting data....')
-    data, other_tags = hashtag_data(hashtag)
-    print('Done...')
+    data = insta_scraper(tag)
+    print('Data collection has been completed.')
